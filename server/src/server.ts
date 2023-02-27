76b0373ee5aa92630d6e547d7ea224f5612e5b1c
Kevin Malby <kevin.malby@xemelgo.com>
2023-02-26 20:02:57 -0800

    Adding eslint setup and skeleton of note routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,13 @@
-import express, { Express, Request, Response } from "express";
 import * as dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import noteRoutes from "./routes/note.routes.js";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Test');
-});
+app.use("/notes", noteRoutes);
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
